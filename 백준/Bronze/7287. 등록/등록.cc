84557cb76ba891bc9464402d2c6e6cc02b2b54a2
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2024-03-09 19:55:41 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    int correctcount = 111;
+    std::string id = "sm524233";
+    std::cout << correctcount << std::endl;
+    std::cout << id;
+    return 0;
+}
