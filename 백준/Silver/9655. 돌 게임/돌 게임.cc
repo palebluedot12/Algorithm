e29e2dd70e0ddfc1a034d6a44980b5faa665b582
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2024-03-31 20:20:28 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void){
+    int rocks;
+	cin >> rocks;
+    
+	if (rocks % 2 == 1) {
+		cout << "SK";
+	}
+
+	else {
+		cout << "CY";
+	}
+    
+}
