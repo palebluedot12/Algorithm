57a745a1ff99969a014e1039f6fe6f93cc2ae372
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2024-07-03 16:31:07 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-	int M = 1234567891;
-	int r = 31;
+	long long M = 1234567891;
+	long long r = 1;
 
 	int length;
-	int sum = 0;
+	unsigned long long sum = 0;
 
 	cin >> length;
 	char* string = new char[length];
@@ -19,9 +19,11 @@ int main()
 		cin >> string[i];
 	}
 
+	// sum 이 너무 큰 탓...
 	for (size_t i = 0; i < length; i++)
 	{
-		sum += (int(string[i]) - 96) * pow(r,i);
+		sum += ((int(string[i]) - 96) * r) % M;
+		r = (r * 31) % M;
 	}
 
 	cout << sum % M;
