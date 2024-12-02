6ed127d2d3a56bbb9bf9ac903fa3f81d0a687901
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2024-12-02 19:17:54 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.05 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -4,28 +4,15 @@
 
 using namespace std;
 
-// Compare 클래스를 사용하여 n을 전달
-class Compare {
-public:
-    int n; 
-
-    Compare(int index) : n(index) {}  // 생성자에서 n을 초기화
-
-    bool operator()(const string& a, const string& b) const 
-    {
-        if (a[n] != b[n]) return a[n] < b[n];
-        return a < b;
-    }
-};
+int i;
 
 bool compare(const string& a, const string& b)
 {
-    int n = 1;
-    if (a[n] != b[n]) return a[n] < b[n];
+    if (a[i] != b[i]) return a[i] < b[i];
     return a < b;
 }
 vector<string> solution(vector<string> strings, int n) {
-    Compare compare(n);
+    i = n;
     sort(strings.begin(), strings.end(), compare);
     return strings;
 }
