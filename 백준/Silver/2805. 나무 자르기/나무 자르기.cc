3fcc3f8cf6a21ed9e245a1ba6e8d32268cc5bfaa
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2025-04-13 00:04:20 +0900

    [Silver II] Title: 나무 자르기, Time: 588 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long tree[1000005]; // 나무
+
+int n;
+long long m;
+
+bool solve(long long x)
+{
+	long long sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (tree[i] >= x)
+		{
+			sum += tree[i] - x;
+		}
+	}
+	return sum >= m; // 개수가 n개보다 많아지는 시점
+}
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> tree[i];
+
+	long long st = 0;
+	long long en = 1000000000;
+
+	while (st < en)
+	{
+		long long mid = (st + en + 1) / 2;
+		if (solve(mid)) st = mid;
+		else en = mid - 1;
+	}
+	cout << st;
+}
