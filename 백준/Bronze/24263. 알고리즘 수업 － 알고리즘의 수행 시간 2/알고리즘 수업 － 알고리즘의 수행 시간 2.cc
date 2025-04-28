0a0cc72473c7d0c5ce61499e1bcdb98d54034dd5
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2025-04-28 19:39:51 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   
+    int n;
+    cin >> n;
+    cout << n << '\n';   // 수행 횟수
+    cout << 1 << '\n';   // 최고차항 차수
+    return 0;    
+}
