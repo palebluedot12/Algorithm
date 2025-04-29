94cfa908684fc620072619d845c4ce666bd45bed
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2025-04-30 04:59:22 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[5];
+int sum = 0;
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arr[i];
+		sum += arr[i];
+	}
+		
+	sort(arr, arr + 5);
+	
+	cout << sum / 5 << '\n';
+	cout << arr[2];
+}
