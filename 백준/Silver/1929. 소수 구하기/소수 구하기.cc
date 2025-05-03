4526846256efd8a9b615978056b34a880a990a54
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2025-05-03 15:10:17 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2260 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int m, n;
+    cin >> m >> n;
+
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; ++i) 
+    {
+        if (isPrime[i]) 
+        {
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+
+    for (int i = m; i <= n; ++i) 
+    {
+        if (isPrime[i])
+            cout << i << '\n';
+    }
+
+    return 0;
+}
