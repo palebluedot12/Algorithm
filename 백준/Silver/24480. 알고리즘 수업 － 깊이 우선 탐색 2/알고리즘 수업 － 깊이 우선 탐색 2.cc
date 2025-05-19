3b5ca8093572bb7e05199860edd1eea570062c64
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2025-05-20 02:45:29 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 88 ms, Memory: 11028 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[100005];
+bool vis[100005];
+int order[100005];
+int cnt = 1;
+
+void dfs(int cur)
+{
+    vis[cur] = true;
+    order[cur] = cnt++;
+
+    for (int nxt : adj[cur])
+    {
+        if (!vis[nxt]) dfs(nxt);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int v, e, r;
+    cin >> v >> e >> r;
+
+    for (int i = 0; i < e; i++)
+    {
+        int v1, v2;
+        cin >> v1 >> v2;
+        adj[v1].push_back(v2);
+        adj[v2].push_back(v1);
+    }
+
+    for (int i = 1; i <= v; i++) {
+        sort(adj[i].begin(), adj[i].end(), greater<int>());
+    }
+
+    dfs(r);
+
+    for (int i = 1; i <= v; i++) {
+        cout << order[i] << '\n';
+    }
+
+    return 0;
+}
