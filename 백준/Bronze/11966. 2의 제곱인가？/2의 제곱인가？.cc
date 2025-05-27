a6536656ac2ba2b5fddc7b0aa92ea4ad559bfe40
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2025-05-28 04:59:18 +0900

    [Bronze III] Title: 2의 제곱인가?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11966. 2의 제곱인가？/2의 제곱인가？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N;
+    cin >> N;
+
+    if (N > 0 && (N & (N - 1)) == 0)
+        cout << 1;
+    else
+        cout << 0;
+
+    return 0;
+}
