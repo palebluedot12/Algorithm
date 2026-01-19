1709418432435d97d341ce27482ebe0f740f73ec
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2026-01-19 11:40:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -4,13 +4,6 @@
 using namespace std;
 
 bool solution(int x) {
-    int sum = 0;
-    int temp = x;
-
-    while (temp > 0) {
-        sum += temp % 10; 
-        temp /= 10;       
-    }
-
-    return (x % sum == 0);
+    bool answer = true;
+    return answer;
 }
