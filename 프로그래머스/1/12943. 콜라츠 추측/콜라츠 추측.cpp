88b5d17ad718d8b9fb49ac71a7071ec8a13ca97c
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2026-01-21 15:21:40 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    long long n = num; 
+
+    for (int i = 0; i < 500; ++i) {
+        if (n == 1) { return i;}
+
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+    }
+
+    return -1;
+}
