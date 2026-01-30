5d6d1a5cc91d4c35c4ceab571ca89b3817199b14
Sangmin Kim <64486585+palebluedot12@users.noreply.github.com>
2026-01-30 11:07:15 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s) {
+    int len = s.length();
+    int mid = len / 2;
+    
+    if (len % 2 == 0) {
+        return s.substr(mid - 1, 2);
+    } 
+    else {
+        return s.substr(mid, 1);
+    }
+}
